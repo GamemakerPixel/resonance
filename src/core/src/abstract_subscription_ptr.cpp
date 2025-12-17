19871b7536dd977f42b982cbb17763b390bbffd7
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-12-16 21:32:13 -0500

    Remove header comment in AbstractSubscriptionPtr implementation

--- src/core/src/abstract_subscription_ptr.cpp
+++ src/core/src/abstract_subscription_ptr.cpp
@@ -66,38 +66,5 @@ void
 }
 
 
-template class AbstractSubscriptionPtr<AudioBackend>;
-template class AbstractSubscriptionPtr<AudioOutputDevice>;
-
-
-/*
-class AbstractDepender;
-
-// Will act as a shared ptr to an object until all objects passed as dependers of the
-// object are deconstructed (they will notify this ptr when that happens, it will keep
-// a reference count). After this, it will act as a weak ptr. It will not act as a
-// shared ptr until it has subscribed to at least one depender.
-template <typename T>
-class AbstractSubscriptionPtr
-{
-private:
-  std::shared_ptr<T> m_shared = nullptr;
-  std::weak_ptr<T> m_weak;
-  int m_living_depender_count = 0;
-
-public:
-  // Note that ownership of the object isn't taken until subscribe_to_depender is called
-  // at least once.
-  AbstractSubscriptionPtr(std::shared_ptr<T> object);
-
-  void
-    subscribe_to_depender(AbstractDepender& depender);
-
-  std::weak_ptr<T>
-    get_object();
-
-private:
-  void
-    on_depender_deconstructed();
-};
-*/
+template class resonance_core::AbstractSubscriptionPtr<AudioBackend>;
+template class resonance_core::AbstractSubscriptionPtr<AudioOutputDevice>;
